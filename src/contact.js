3ca25bbae3d05e4ab7e2c36dec792b3b5b4fb897
ak2783934 <ak2783934@gmail.com>
2021-01-12 16:13:25 +0530

    added

--- src/contact.js
+++ src/contact.js
@@ -6,58 +6,74 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import { Col, Row } from "react-bootstrap";
 
 const encode = (data) => {
-    return Object.keys(data)
-        .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
-        .join("&");
-  };
+  return Object.keys(data)
+    .map((key) => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+    .join("&");
+};
 
 class Contacts extends Component {
-    constructor(props) {
-        super(props);
-        this.state = { name: "", email: "", message: "" };
-      }
-  
-      /* Here’s the juicy bit for posting the form submission */
-  
-      handleSubmit = e => {
-        fetch("/", {
-          method: "POST",
-          headers: { "Content-Type": "application/x-www-form-urlencoded" },
-          body: encode({ "form-name": "contact", ...this.state })
-        })
-          .then(() => alert("Success!"))
-          .catch(error => alert(error));
-  
-        e.preventDefault();
-      };
-  
-      handleChange = e => this.setState({ [e.target.name]: e.target.value });
-  
-      render() {
-        const { name, email, message } = this.state;
-        return (
-          <form onSubmit={this.handleSubmit}>
-            <p>
-              <label>
-                Your Name: <input type="text" name="name" value={name} onChange={this.handleChange} />
-              </label>
-            </p>
-            <p>
-              <label>
-                Your Email: <input type="email" name="email" value={email} onChange={this.handleChange} />
-              </label>
-            </p>
-            <p>
-              <label>
-                Message: <textarea name="message" value={message} onChange={this.handleChange} />
-              </label>
-            </p>
-            <p>
-              <button type="submit">Send</button>
-            </p>
-          </form>
-        );
-      }
+  constructor(props) {
+    super(props);
+    this.state = { name: "", email: "", message: "" };
+  }
+
+  /* Here’s the juicy bit for posting the form submission */
+
+  handleSubmit = (e) => {
+    fetch("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: encode({ "form-name": "contact", ...this.state }),
+    })
+      .then(() => alert("Success!"))
+      .catch((error) => alert(error));
+
+    e.preventDefault();
+  };
+
+  handleChange = (e) => this.setState({ [e.target.name]: e.target.value });
+
+  render() {
+    const { name, email, message } = this.state;
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <p>
+          <label>
+            Your Name:{" "}
+            <input
+              type="text"
+              name="name"
+              value={name}
+              onChange={this.handleChange}
+            />
+          </label>
+        </p>
+        <p>
+          <label>
+            Your Email:{" "}
+            <input
+              type="email"
+              name="email"
+              value={email}
+              onChange={this.handleChange}
+            />
+          </label>
+        </p>
+        <p>
+          <label>
+            Message:{" "}
+            <textarea
+              name="message"
+              value={message}
+              onChange={this.handleChange}
+            />
+          </label>
+        </p>
+        <p>
+          <button type="submit">Send</button>
+        </p>
+      </form>
+    );
   }
 }
 
